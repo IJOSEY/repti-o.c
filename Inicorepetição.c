c84f929992aa940f92d5b5f40c718ae0fe627caa
IJOSEY <ijoseybs@GMAIL.COM>
2025-02-10 13:00:04 +0000

    correção de erro.

--- Inicorepetição.c
+++ Inicorepetição.c
@@ -1,1 +1,12 @@
-inicio da jornada!
+#include <stdio.h>
+int main(){
+    int num;
+    printf("Digite valor inicial do loop.\n");
+    scanf("%d",&num);
+    while (num <= 100)
+    {
+        printf("num: %d\n",num);
+        num++;
+    }
+    
+}
